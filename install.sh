b96eec89ed690ccd752a558ebe3cc3f7ed8d3298
eyal <eyalqwert@gmail.com>
2022-02-07 10:43:05 -0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y install python3
+sudo apt -y install pip3
+sudo apt -y install gcc
+sudo apt -y install openssh-server
+pip install scapy
+pip install python-iptables
